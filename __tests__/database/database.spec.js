18854cd3ae7de8a61db5363ae33ea4877b8d7e04
Igor Vinicius <igorvinicius.oli@gmail.com>
2024-04-18 08:23:24 -0300

    feat: add database to control save data flow

---
+++ __tests__/database/database.spec.js
@@ -0,0 +1,132 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { LocalDatabase } from '../../src/database/database';
+
+const databasePath = path.resolve(__dirname, '../../db.json');
+
+describe('Database', () => {
+    afterAll(() => {
+        if(fs.existsSync('./db.json')) {
+            fs.unlinkSync('./db.json');
+        }
+    });
+
+    it('should create a database file', done => {
+        new LocalDatabase();
+
+        setTimeout(() => {
+            expect(fs.existsSync('./db.json')).toBe(true);
+            done();
+        }, 1000);
+    });
+    it('should create a database file', () => {
+        const database = new LocalDatabase();
+        
+        expect(fs.existsSync('./db.json')).toBe(true);
+    });
+
+    it('should insert data', () => {
+        const database = new LocalDatabase();
+        const data = database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        database.insert('users', {
+            name: 'Jane Doe',
+            email: 'jane.doe@email.com',
+        });
+
+        expect(data).toHaveProperty('id');
+        expect(data).toHaveProperty('createdAt');
+        expect(data).toHaveProperty('updatedAt');
+        expect(data.name).toBe('John Doe');
+        expect(data.email).toBe('john.doe@email.com');
+    });
+
+    it('should select data', () => {
+        const database = new LocalDatabase();
+
+        database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        const data = database.select('users');
+
+        expect(data).toHaveLength(1);
+    });
+
+    it('should select data with two params', () => {
+        const database = new LocalDatabase();
+
+        database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        const data = database.select('users', { name: 'John Doe', email: 'john.doe@email.com' });
+
+        expect(data).toHaveLength(1);
+    });
+
+    it('should select data with id', () => {
+        const database = new LocalDatabase();
+
+        const user = database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+        
+        const [data] = database.select('users', { id: user.id });
+
+        expect(data).toBe(user);
+    });
+
+    it('should update data', () => {
+        const database = new LocalDatabase();
+
+        const user = database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        const updatedUser = database.update('users', user.id, {
+            name: 'Jane Doe',
+        });
+
+        expect(updatedUser.name).toEqual('Jane Doe');
+        expect(updatedUser.id).toEqual(user.id);
+    });
+
+    it('should return null if data is not found', () => {
+        const database = new LocalDatabase();
+
+        const user = database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        const updatedData = database.update('users', '1', {
+            name: 'Jane Doe',
+        });
+
+        expect(updatedData).toBeNull();
+    });
+
+    it('should delete data', () => {
+        const database = new LocalDatabase();
+
+        const user = database.insert('users', {
+            name: 'John Doe',
+            email: 'john.doe@email.com',
+        });
+
+        database.delete('users', user.id);
+
+        const search = database.select('users', { id: user.id });
+
+        expect(search).toHaveLength(0);
+    });
+});
---
+++ src/database/database.js
@@ -0,0 +1,90 @@
+import fs from 'node:fs/promises';
+import { randomUUID as uuid } from 'node:crypto';
+import path from 'node:path';
+
+const databasePath = path.resolve(__dirname, '../../db.json');
+
+export class LocalDatabase {
+    #database = {};
+	
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+            .then(data => {
+                this.#database = JSON.parse(data);
+            })
+            .catch(() => {
+                this.#persist();
+            });
+    }
+
+    async #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    }
+
+    insert(table, data) {
+		const insertData = {
+			...data,
+			id: uuid(),
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+
+        if(Array.isArray(this.#database[table])) {
+			this.#database[table].push(insertData);
+		} else {
+			this.#database[table] = [insertData];
+		}
+	
+		this.#persist()
+		
+		return insertData;  
+    }
+
+    select(table, search) {
+		let data = this.#database[table] ?? [];
+
+		if(search && Object.entries(search).length > 1 && data.length > 0) {
+			data = data.filter(row => {
+				return Object.entries(search).some(([key, value]) => {
+					return row[key]
+						.toLowerCase()
+						.includes(value.toLowerCase() ?? '');
+				});
+			});
+		}
+
+		return data;
+	}
+
+	update(table, id, data) {
+		const rowIndex = this.#database[table].findIndex(row => row.id == id);
+
+		let updateData = null;
+
+		if (rowIndex > -1) 
+		{
+			updateData = {
+				...this.#database[table][rowIndex],
+				...data,
+				updatedAt: new Date(),
+			};
+
+			this.#database[table][rowIndex] = updateData;
+
+			this.#persist();
+		}
+
+		return updateData;
+	}
+
+	delete(table, id) {
+		const rowIndex = this.#database[table].findIndex(row => row.id == id);
+
+		if (rowIndex > -1) {
+			this.#database[table].splice(rowIndex, 1);
+
+			this.#persist();
+		}
+	}
+
+}
